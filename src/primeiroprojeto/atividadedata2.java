dd56094e4dad21463335207cd6810a2f7a22de8b
deivison.dick <deivisondick94@gmail.com>
2026-03-19 21:14:27 -0300

    atualizando atividadedata2

--- src/primeiroprojeto/atividadedata2.java
+++ src/primeiroprojeto/atividadedata2.java
@@ -1,5 +1,33 @@
 package primeiroprojeto;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class atividadedata2 {
+	
+	public static void main(String[] args) {
+		contaTempo( 04, 01,2027 );
+	}
+	private static void contaTempo(int dia, int mes, int ano) {
+		
+		LocalDate agora = LocalDate.now();
+		LocalDate segundaData = LocalDate.of(ano, mes, dia);
+		
+		Period periodo = Period.between(agora, segundaData);
+		
+		int dias = periodo.getDays();
+		int meses = periodo.getMonths();
+		int anos = periodo.getYears();
+		
+		System.out.println("A diferença é " + anos + " ano(s), " 
+				+ meses + " mes(es) e"		
+				+ dias + " dia(s). ");
+
+		
+		
+	}
+	
+	
+	
 
 }
